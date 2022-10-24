985d13ec196f83534034dd12a9cc9a9bcc5e62ce
seanlin1125 <sean821125@gmail.com>
2022-10-24 15:55:30 +0800

    feat: add login feature via passport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
@@ -7,10 +8,24 @@ const PORT = 3000
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+// 引用 passport，放在文件上方
+const passport = require('passport')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 app.get('/', (req, res) => {
   return Todo.findAll({
     raw: true,
@@ -19,15 +34,22 @@ app.get('/', (req, res) => {
     .then((todos) => { return res.render('index', { todos }) })
     .catch((error) => { return res.status(422).json(error) })
 })
+
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
+
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
+// 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.post('/users/login', (req, res) => {
   res.send('login')
@@ -65,6 +87,7 @@ app.post('/users/register', (req, res) => {
 app.get('/users/logout', (req, res) => {
   res.send('logout')
 })
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
