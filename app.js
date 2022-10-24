cb8f0e9edce0f89c5b5b9b5103d316a947642ed6
seanlin1125 <sean821125@gmail.com>
2022-10-24 15:31:07 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.get('/', (req, res) => {
     .then((todos) => { return res.render('index', { todos }) })
     .catch((error) => { return res.status(422).json(error) })
 })
-
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
